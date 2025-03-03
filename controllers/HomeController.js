a8d7d2fc4fdd2b754badec2335b7ed2e4ef84468
Cong Wang <cwang167@my.centennialcollege.ca>
2025-03-03 13:50:17 -0500

    dall-e

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,4 +1,7 @@
 import OpenAI from 'openai'
+import fs from 'fs'
+import multer from 'multer'
+
 
 class HomeController{
     static chatGPTCompletion = async (req, res) => {
@@ -22,6 +25,59 @@ class HomeController{
             return res.status(200).send(completion)
         }catch (e) {
             console.log(e)
+            return res.status(500).send(e)
+        }
+    }
+
+    static dalleImageGenerator = async (req, res) => {
+
+        const { prompt } = req.body
+
+        // const prompt = "A cute baby sea otter"
+
+        try{
+            const openai = new OpenAI({
+                apiKey: process.env.OPENAI_KEY
+            })
+
+            const response =  await openai.images.generate({
+                model: "dall-e-2",
+                prompt,
+                n: 4,
+                size: '1024x1024'
+            })
+
+            return res.status(200).send(response.data)
+
+        }catch (e) {
+            console.log(e)
+            return res.status(500).send(e)
+        }
+    }
+
+    static dalleImageEditor = async (req, res) => {
+
+        const imagePath = req.file.path
+
+
+        try{
+            const openai = new OpenAI({
+                apiKey: process.env.OPENAI_KEY
+            })
+
+            const response =  await openai.images.createVariation({
+                model: "dall-e-2",
+                image: fs.createReadStream(imagePath),
+                n: 4
+            })
+
+            console.log(response)
+
+            return res.status(200).send(response.data)
+
+        }catch (e) {
+            console.log(e)
+            return res.status(500).send(e)
         }
     }
 }
--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -1,8 +1,28 @@
 import {Router} from 'express'
 import HomeController from "../controllers/HomeController.js";
+import multer from "multer";
 
 const homeRouter = Router()
 
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/upload')
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + file.originalname
+        cb(null, uniqueSuffix)
+    }
+})
+
+
+const upload = multer({
+    storage
+})
+
 homeRouter.post('/completions', HomeController.chatGPTCompletion)
+homeRouter.post('/images', HomeController.dalleImageGenerator)
+homeRouter.post('/images/upload', upload.single('image'), HomeController.dalleImageEditor)
+// homeRouter.post('/images/variations', HomeController.dalleImagevariations)
 
 export default homeRouter
